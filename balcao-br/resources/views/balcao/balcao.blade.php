003f8ad333e9de80b79fc57988503d003300cc06
Bruno de Souza <brunohyro@hotmail.com>
2022-09-19 15:08:46 -0300

    att balcao

--- balcao-br/resources/views/balcao/balcao.blade.php
+++ balcao-br/resources/views/balcao/balcao.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-  <link href="css/balcao/style.css" rel="stylesheet"/>
+  <link href="/public/assets/balcao/style.css" rel="stylesheet"/>
   <link rel="icon" href="/public/assets/layouts/img/brand/brand_256x256.jpg">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://kit.fontawesome.com/f359eececc.js" crossorigin="anonymous"></script>
@@ -20,11 +20,17 @@
       </form>
       <ul>
         <!-- <li> <i class="fa-solid fa-user"></i> <a href="#">Cadastro</a> </li> -->
-        <a href="#"><span class="fa-solid fa-user"></span></a>
+        <div class="tooltip">
+        <a href="#"><span class="fa-solid fa-user"><span class="tooltiptext">Login</span></span></a>
+        </div>
         <!-- <li> <i class="fa-solid fa-address-card"></i> <a href="#">Termos de uso</a></li> -->
-        <a href="#"><span class="fa-solid fa-address-card"></span></a>
+        <div class="tooltip">
+        <a href="#"><span class="fa-solid fa-address-card"><span class="tooltiptext">Termos de Uso</span></span></a>
+        </div>
         <!-- <li> <i class="fa-solid fa-cart-shopping"></i> <a href="#">Catálogo</a></li> -->
-        <a href="#"><span class="fa-solid fa-cart-shopping"></span></a>
+        <div class="tooltip">
+        <a href="#"><span class="fa-solid fa-cart-shopping"><span class="tooltiptext">Catálogo</span></span></a>
+        </div>
       </ul>
     </header>
 
@@ -42,7 +48,7 @@
         <div class="left box">
           <h2> Sobre o projeto</h2>
           <div class="content">
-          <p> O Balcão Brasil foi idealizado para ter uma opção ainda mais ampla quando falamos de venda e troca de mercadorias. Hoje no mercado temos muitas opções de sites de intermédio, de compra e venda. Mas, decidimos ir um pouco contra a maré, dando poder para o comprador informar sua necessidade, e o vendedor da mercadoria correr atrás do seu cliente!</p>
+            <p> O Balcão Brasil foi idealizado para ter uma opção ainda mais ampla quando falamos de venda e troca de mercadorias. Hoje no mercado temos muitas opções de sites de intermédio, de compra e venda. Mas, decidimos ir um pouco contra a maré, dando poder para o comprador informar sua necessidade, e o vendedor da mercadoria correr atrás do seu cliente!</p>
           
             <div class="social">
               <!-- <a href="#"><span class="fab fa-facebook-f"></span></a>
