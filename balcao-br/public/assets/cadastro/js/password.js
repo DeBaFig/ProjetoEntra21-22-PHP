8b85366edfdd7ee7a6e715143650aa26d470944e
Jennifer Barbosa <jennifer_lye@hotmail.com>
2022-09-19 12:23:43 -0300

    colocando visualizar senha

---
+++ balcao-br/public/assets/cadastro/js/password.js
@@ -0,0 +1,20 @@
+
+
+$('.ueEyePass').click(function(e){
+    var target = e.currentTarget
+    $(target).hasClass('show')?hidePassword($(target)):showPassword($(target))
+    
+})
+function hidePassword(e){
+    e.removeClass('show').addClass('hide')
+    
+    $("#ueEyePass").removeClass('fa-eye-slash').addClass('fa-eye')
+    $("#password").attr('type','password')
+    console.log('hidePassword')
+}
+function showPassword(e){
+    e.removeClass('hide').addClass('show')
+    $("#ueEyePass").removeClass('fa-eye').addClass('fa-eye-slash')
+    $("#password").attr('type','text')
+    console.log('showPassword')
+}
--- balcao-br/resources/views/cadastro/cadastro.blade.php
+++ balcao-br/resources/views/cadastro/cadastro.blade.php
@@ -7,8 +7,10 @@
 
   <title>Cadastro</title>
   <link rel="stylesheet" href="/assets/cadastro/css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
+
 <body>
   <div class="center">
     <h1>Crie sua conta</h1>
@@ -19,7 +21,7 @@
         <label>Full name</label>
       </div>
       <div class="text-fild">
-        <input type="email" required>
+        <input type="mailto" required>
         <span></span>
         <label>Email</label>
       </div>
@@ -28,25 +30,30 @@
         <span></span>
         <label>Password</label>
       </div>
-      <div class="text-fild">
-        <input type="password" required>
+      <div class="text-fild submit-line">
+        <button class="submit-eye">
+          <i id="ueEyePass" class="fa fa-eye ueEyePass"></i>
+         </button>
+        <input id="password" type="password" required> 
         <span></span>
         <label>Confirm Password</label>
       </div>
-      <div>
-      <input type="checkbox" id="horns" name="horns">
-      <label for="horns">I have read and agreed to the Terms of Service and Privacy Policy</label>
-     </div>
-      <!-- <div class="pass"></div> -->
+      <div class="container">
+        <input type="checkbox" id="horns" name="aceite">
+        <label for="horns">I have read and agreed to the Terms of Service and Privacy Policy</label>
+      </div>
       <input class="btn" type="submit" value="Criar conta">
       <div class="pass">Already have an account? Log In</div>
-      </div>
+  </div>
   </div>
   </form>
   <div class="center-2">
     <h2>Balcão Brasil</h2>
     <img src="/assets/cadastro/img/lazy.png" alt="logo-balcao" height="350" width="350" />
-      </div>
+  </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="/assets/cadastro/js/password.js"></script>
 </body>
 
 </html>
