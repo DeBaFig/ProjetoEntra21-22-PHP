a79fbd2dda4aa78dbde309dce80a01b59fc7a023
Icaromelchior <icaro_barbosa@outlook.com>
2022-09-19 05:03:24 -0300

    login com breeze

--- balcao-br/routes/web.php
+++ balcao-br/routes/web.php
@@ -13,5 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\HomeController@building')->name("building.index");
-Route::get('/login', 'App\Http\Controllers\LoginController@home')->name("home.login");
+Route::get('/', function () {
+  return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+  return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
