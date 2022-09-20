585b13b4d3bae4e4a78b693214a09099bd195e11
Denize Bassi Figueiredo <denize.f.bassi@gmail.com>
2022-09-19 21:31:51 -0300

    adição layout

---
+++ balcao-br/resources/views/layouts/appMaster.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ asset('assets/layouts/css/style.css') }}">
+    <link rel="icon" href="{{ asset('assets/layouts/img/brand/brand_256x256.jpg') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/6e7aa9ccc9.js" crossorigin="anonymous"></script>
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div id="app">
+        @yield('layoutContent')
+    </div>
+</body>
+</html>
---
+++ balcao-br/resources/views/layouts/footer.blade.php
@@ -0,0 +1,30 @@
+
+<footer class="content-footer footer bg-footer-theme fixed-bottom">
+    <div class="container row">
+        <div class="col-4">
+            <h2> Sobre o projeto</h2>
+            <div class="content">
+                <p> O Balcão Brasil foi idealizado para ter uma opção ainda mais ampla quando falamos de venda e troca de mercadorias. Hoje no mercado temos muitas opções de sites de intermédio, de compra e venda. Mas, decidimos ir um pouco contra a maré, dando poder para o comprador informar sua necessidade, e o vendedor da mercadoria correr atrás do seu cliente!</p>
+            </div>
+        </div>
+        <div class="col-4">
+            <h2>Endereço</h2>
+            <div class="content">
+                <div class="place">
+                    <span class="fas fa-map-marker"></span>
+                    <span class="text">Brasil, Santa Catarina</span>
+                </div>
+
+                <div class="phone">
+                    <span class="fas fas fa-phone-alt"></span>
+                    <span class="text">+55 47 9999-9999</span>
+                </div>
+
+                <div class="email">
+                    <span class="fas fa-envelope"></span>
+                    <span class="text">exemplo@exemplo.com</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
