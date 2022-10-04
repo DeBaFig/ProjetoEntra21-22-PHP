fb8d953cd82f06bebc388085c8fd7e7dbe38fdef
Denize Bassi Figueiredo <denize.f.bassi@gmail.com>
2022-10-04 20:03:00 -0300

    Melhoria do Còdigo para diminuir a repetição - about feito bruno melhorado Denize

--- remake/app/Http/Controllers/HomeController.php
+++ remake/app/Http/Controllers/HomeController.php
@@ -8,17 +8,33 @@ use Auth;
 
 class HomeController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
 
         return view('home.index');
     }
-    public function about(){
-        return view('home.about');
+    public function about()
+    {
+        $viewData = [];
+        $viewData['info'] =
+            [
+                "denize" => ['name' => "Denize Bassi Figueiredo", 'city' => "Joinville - SC", 'img' => "denize.jpg", 'desc' => "Desde jovem sou atraída por tecnologia, minhas primeiras linhas de código foram em VisualBasic, hoje mais madura, acumulando uma variedade de cursos, mas também uma desenvoltura profissional focada em auto-desenvolvimento e compartilhamento de conhecimentos. Minha motivação é zelar por minha família e constantemente aprender algo novo.", 'link' => "https://github.com/DeBaFig/"],
+                "Icaro" => ['name' => "", 'city' => "", 'img' => "icaro.jpg", 'desc' => "", 'link' => ""],
+                "Bruno" => ['name' => "Bruno de Souza", 'city' => "Penha - SC", 'img' => "bruno.jpg", 'desc' => "Nascido em Belo Horizonte (MG) há 18 anos. Interesse por informática e programação desde muito tempo, e trabalhando na área de hardware há quase dois anos. Participar do Entra21 me despertou mais vontade de aprender e progredir na área! Atualamente também cursando superior de Análise e Desenvolvimento de Sistemas.", 'link' => "https://github.com/brwnosouza"],
+                "Paula" => ['name' => "", 'city' => "", 'img' => "paula.jpg", 'desc' => "", 'link' => ""],
+                "Jennifer" => ['name' => "", 'city' => "", 'img' => "jennifer.jpg", 'desc' => "", 'link' => ""],
+                "Lucas" => ['name' => "", 'city' => "", 'img' => "lucas.jpg", 'desc' => "", 'link' => ""],
+                "Adriano" => ['name' => "", 'city' => "", 'img' => "adriano.jpg", 'desc' => "", 'link' => ""]
+            ];
+
+        return view('home.about')->with("viewData", $viewData);
     }
-    public function aboutBalcao(){
+    public function aboutBalcao()
+    {
         return view('home.aboutBalcaoBrasil');
     }
-    public function contact(){
+    public function contact()
+    {
         return view('home.contact');
     }
 }
--- remake/resources/views/home/about.blade.php
+++ remake/resources/views/home/about.blade.php
@@ -9,15 +9,21 @@
                         <p>
                     </div>
                     <div class="flex flex-wrap -m-4 justify-center">
+
+                        @foreach($viewData['info'] as $key => $detalhes)
                         <div class="p-4 lg:w-1/2">
                             <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                                <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4 shadow-2xl" src="/remake/public/assets/about/img/denize.jpg">
+                                <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4 shadow-2xl" src="{{ asset('assets/img/' . $detalhes['img'] )}}">
                                 <div class="flex-grow sm:pl-8">
-                                    <h2 class="title-font font-medium text-lg text-black">Denize Bassi Figueiredo</h2>
-                                    <h3 class="text-black-500 mb-3">Joinville - SC</h3>
-                                    <p class="mb-4">BREVE APRESENTAÇÃO</p>
+                                    <h2 class="title-font font-medium text-lg text-black">
+                                        {{ $detalhes['name']}}
+                                    </h2>
+                                    <h3 class="text-black-500 mb-3">
+                                        {{ $detalhes['city']}}
+                                    </h3>
+                                    <p class="mb-4">{{ $detalhes['desc']}}</p>
                                     <span class="inline-flex">
-                                        <a class="rounded-lg text-slate-900 font-semibold transition flex items-center gap-3 text-[0.8125rem] leading-6 py-1 px-1.5 hover:bg-slate-900/[0.03] -my-1 -mx-1.5" href="https://github.com/DeBaFig">
+                                        <a class="rounded-lg text-slate-900 font-semibold transition flex items-center gap-3 text-[0.8125rem] leading-6 py-1 px-1.5 hover:bg-slate-900/[0.03] -my-1 -mx-1.5" href="{{ $detalhes['link']}}">
                                             <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 fill-slate-900">
                                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"></path>
                                             </svg>GitHub</a>
@@ -25,113 +31,6 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="p-4 lg:w-1/2">
-                            <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                                <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4 shadow-2xl" src="/remake/public/assets/about/img/icaro.jpg">
-                                <div class="flex-grow sm:pl-8">
-                                    <h2 class="title-font font-medium text-lg text-black">Icaro Melchior</h2>
-                                    <h3 class="text-black-500 mb-3">Balneário Camboriú - SC</h3>
-                                    <p class="mb-4">BREVE APRESENTAÇÃO</p>
-                                    <span class="inline-flex">
-                                        <a class="rounded-lg text-slate-900 font-semibold transition flex items-center gap-3 text-[0.8125rem] leading-6 py-1 px-1.5 hover:bg-slate-900/[0.03] -my-1 -mx-1.5" href="https://github.com/Icaromelchior">
-                                            <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 fill-slate-900">
-                                                <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"></path>
-                                            </svg>GitHub</a>
-                                    </span>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="p-4 lg:w-1/2">
-                            <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                                <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4 shadow-2xl" src="/remake/public/assets/about/img/jennifer.jpg">
-                                <div class="flex-grow sm:pl-8">
-                                    <h2 class="title-font font-medium text-lg text-black">Jennifer Barbosa</h2>
-                                    <h3 class="text-black-500 mb-3">Brusque - SC</h3>
-                                    <p class="mb-4">BREVE APRESENTAÇÃO</p>
-                                    <span class="inline-flex">
-                                        <a class="rounded-lg text-slate-900 font-semibold transition flex items-center gap-3 text-[0.8125rem] leading-6 py-1 px-1.5 hover:bg-slate-900/[0.03] -my-1 -mx-1.5" href="https://github.com/jennibarbosa">
-                                            <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 fill-slate-900">
-                                                <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"></path>
-                                            </svg>GitHub</a>
-                                    </span>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="p-4 lg:w-1/2">
-                            <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                                <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4 shadow-2xl" src="/remake/public/assets/about/img/paula.jpg">
-                                <div class="flex-grow sm:pl-8">
-                                    <h2 class="title-font font-medium text-lg text-black">Paula Eduarda Silva</h2>
-                                    <h3 class="text-black-500 mb-3">Xanxerê - SC</h3>
-                                    <p class="mb-4">BREVE APRESENTAÇÃO</p>
-                                    <span class="inline-flex">
-                                        <a class="rounded-lg text-slate-900 font-semibold transition flex items-center gap-3 text-[0.8125rem] leading-6 py-1 px-1.5 hover:bg-slate-900/[0.03] -my-1 -mx-1.5" href="https://github.com/PaulaKinder">
-                                            <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 fill-slate-900">
-                                                <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"></path>
-                                            </svg>GitHub</a>
-                                    </span>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="flex flex-wrap -m-4 justify-center">
-                            <div class="p-4 lg:w-1/2">
-                                <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                                    <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4 shadow-2xl" src="/remake/public/assets/about/img/lucas.jpg">
-                                    <div class="flex-grow sm:pl-8">
-                                        <h2 class="title-font font-medium text-lg text-black">Lucas Bernadelli dos Passos</h2>
-                                        <h3 class="text-black-500 mb-3">Balneário Camboriú - SC</h3>
-                                        <p class="mb-4">BREVE APRESENTAÇÃO</p>
-                                        <span class="inline-flex">
-                                            <a class="rounded-lg text-slate-900 font-semibold transition flex items-center gap-3 text-[0.8125rem] leading-6 py-1 px-1.5 hover:bg-slate-900/[0.03] -my-1 -mx-1.5" href="https://github.com/PassosLucasB">
-                                                <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 fill-slate-900">
-                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"></path>
-                                                </svg>GitHub</a>
-                                        </span>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="p-4 lg:w-1/2">
-                                <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                                    <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4 shadow-2xl" src="/remake/public/assets/about/img/bruno.jpg">
-                                    <div class="flex-grow sm:pl-8">
-                                        <h2 class="title-font font-medium text-lg text-black">Bruno de Souza</h2>
-                                        <h3 class="text-black-500 mb-3">Penha - SC</h3>
-                                        <p class="mb-4">Nascido em Belo Horizonte (MG) há 18 anos. Interesse por informática e programação desde muito tempo, e trabalhando na área de hardware há quase dois anos. Participar do Entra21 me despertou mais vontade de aprender e progredir na área! Atualamente também cursando superior de Análise e Desenvolvimento de Sistemas.</p>
-                                        <span class="inline-flex">
-                                            <span class="inline-flex">
-                                                <a class="rounded-lg text-slate-900 font-semibold transition flex items-center gap-3 text-[0.8125rem] leading-6 py-1 px-1.5 hover:bg-slate-900/[0.03] -my-1 -mx-1.5" href="https://github.com/brwnosouza">
-                                                    <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 fill-slate-900">
-                                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"></path>
-                                                    </svg>GitHub</a>
-                                            </span>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="p-4 lg:w-1/2">
-                                <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                                    <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4 shadow-2xl" src="/remake/public/assets/about/img/adriano.jpg">
-                                    <div class="flex-grow sm:pl-8">
-                                        <h2 class="title-font font-medium text-lg text-black">Adriano Machado</h2>
-                                        <h3 class="text-black-500 mb-3"> - SC</h3>
-                                        <p class="mb-4">Instrutor da turma de PHP no período vespertino durante as aulas do Entra21 - 2022.</p>
-                                        <span class="inline-flex">
-                                            <a class="rounded-lg text-slate-900 font-semibold transition flex items-center gap-3 text-[0.8125rem] leading-6 py-1 px-1.5 hover:bg-slate-900/[0.03] -my-1 -mx-1.5" href="https://github.com/machado-tec">
-                                                <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 fill-slate-900">
-                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"></path>
-                                                </svg>GitHub</a>
-                                        </span>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
-            </section>
-        </div>
-    </div>
 </x-guest-layout>
