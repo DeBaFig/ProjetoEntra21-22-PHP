6a22945ccdf58572a96fd576aecada78d9b7d642
Denize Bassi Figueiredo <denize.f.bassi@gmail.com>
2022-10-13 18:45:33 -0300

    merge git release-2

--- remake/resources/views/layouts/navigation.blade.php
+++ remake/resources/views/layouts/navigation.blade.php
@@ -199,10 +199,7 @@
             @endif
             <div class="mt-3 space-y-1">
                 <!-- Authentication -->
-                <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link :href="route('user.edit')">
-                    {{ __('TESTE') }}
-                </x-responsive-nav-link>
+                
             </div>
             </div>
         </div>
