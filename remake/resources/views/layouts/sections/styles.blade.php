5cca966b0f6e1d188347148f2ece47171d89cdbb
Denize Bassi Figueiredo <denize.f.bassi@gmail.com>
2022-10-17 19:47:42 -0300

    logout funcionand

--- remake/resources/views/layouts/sections/styles.blade.php
+++ remake/resources/views/layouts/sections/styles.blade.php
@@ -5,7 +5,6 @@
 <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
 <!-- Scripts -->
-@vite(['resources/css/app.css', 'resources/js/app.js'])
 <script src="https://cdn.tailwindcss.com"></script>
 
 
