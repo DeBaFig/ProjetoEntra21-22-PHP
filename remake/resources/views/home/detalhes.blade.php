d86e619e813ff875b28b200eeb98a11f1d8b600f
Paula <paula.kindeer@gmail.com>
2022-10-17 16:42:01 -0300

    fonte pag detalhes

--- remake/resources/views/home/detalhes.blade.php
+++ remake/resources/views/home/detalhes.blade.php
@@ -13,7 +13,7 @@
                                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                                 <title>{{ $product['title'] }}</title>
                                 <link rel = "stylesheet" href = "{{ asset('assets/css/detalhes.css') }}">
-                                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                                <link rel="stylesheet" href="https://fonts.bunny.net/nunito/files/nunito-cyrillic-400-normal.woff2" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
                             </head>
                                 <body>
                                     <div class = "main-wrapper">
@@ -34,8 +34,6 @@
                                             </div>
                                         </div>
                                     </div>
-
-                                    <script src = "detalhes.js"></script>
                                 </body>
                                 </html>
                             @endforeach
